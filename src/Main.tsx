1699f0414e9fec7dae7efe9f1f4ec51204a2fd06
Raymond Santos <ragustinesantos@gmail.com>
2024-11-20 14:59:41 -0700

    11-20-2024 Update: - Implemented navigation UI - PNG files placed in assets

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,16 +1,49 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import React, {useEffect, useState} from 'react';
-import {TouchableOpacity, View, Text, Image} from 'react-native';
-import TrackPlayer, {useProgress, RepeatMode} from 'react-native-track-player';
-import {defaultTrack, TrackObject} from './utils/utility';
+/* eslint-disable react/no-unstable-nested-components */
+import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
-import TestScreen from './screens/TestScreen';
+import Playing from './screens/Playing';
+import HomeStackNav from './navigation/HomeStackNav';
+import PlaylistStackNav from './navigation/PlaylistStackNav';
+import SearchStackNav from './navigation/SearchStackNav';
+import TabIcon from './components/TabIcon';
 
 const AppNav = createBottomTabNavigator();
 
 function Main(): React.JSX.Element {
-  return <TestScreen />;
+  return (
+    <NavigationContainer>
+      <AppNav.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({
+            focused,
+            color,
+            size,
+          }: {
+            focused: boolean;
+            color: string;
+            size: number;
+          }) => {
+            return (
+              <TabIcon
+                route={route.name}
+                focused={focused}
+                color={color}
+                size={size}
+              />
+            );
+          },
+          headerShown: false,
+          tabBarShowLabel: false,
+          tabBarStyle: {backgroundColor: '#222A2C'},
+        })}>
+        <AppNav.Screen name="Home" component={HomeStackNav} />
+        <AppNav.Screen name="Playlists" component={PlaylistStackNav} />
+        <AppNav.Screen name="Playing" component={Playing} />
+        <AppNav.Screen name="Search" component={SearchStackNav} />
+      </AppNav.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default Main;

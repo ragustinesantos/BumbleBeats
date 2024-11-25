1af323f4b828f8d2932fb593abfc06aa2beba351
michaudrd <Renee.Michaud@edu.sait.ca>
2024-11-25 00:03:07 -0700

    11-25-2024 Update: PlaylistCard, ProgressBar, SongListCard,SongModal components and Profile, Playing screens

---
+++ src/components/SongListCard.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+
+type SongListCardParams = {
+  artwork: string,
+  title: string,
+  artist: string,
+};
+
+export default function SongListCard(params : SongListCardParams): React.JSX.Element{
+    const artwork = params.artwork;
+    const title = params.title;
+    const artist = params.artist;
+    
+    return (
+        <View style={styles.container}>
+            <Image source={{ uri: artwork }} style={styles.songArtwork} />
+            <View style={styles.songInfo}>
+                <Text style={styles.songTitle}>{title}</Text>
+                <Text style={styles.songArtist}>{artist}</Text>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 15,
+  },
+
+  songArtwork: {
+    width: 50,
+    height: 50,
+    borderRadius: 8,
+    marginRight: 10,
+  },
+
+  songInfo: {
+    flex: 1,
+  },
+
+  songTitle: {
+    fontSize: 14,
+    fontWeight: '600',
+    color: 'black',
+  },
+
+  songArtist: {
+    fontSize: 12,
+    color: 'black',
+  },
+
+});
+
--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,6 +1,123 @@
 import React from 'react';
-import {View} from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
+import SongCard from '../components/SongCard';
+import SongListCard from '../components/SongListCard';
+
+
+type Song = {
+  id: string;
+  title: string;
+  artist: string;
+  artwork: string;
+};
+
+// Fake data for liked songs -- testing
+const likedSongs: Song[] = [
+  {
+    id: '1',
+    title: 'Song Title',
+    artist: 'Artist',
+    artwork: '',
+  },
+
+];
 
 export default function Profile(): React.JSX.Element {
-  return <View></View>;
+
+  return (
+    <View style={style.container}>
+      <View style={style.profileContainer}>
+        <Image
+          source={{uri: ''}}
+          style={style.profilePic}
+        />
+        <Text style={style.userName}>Name</Text>
+        <View style={style.stats}>
+          <Text style={style.statTxt}>
+            <Text style={style.statNum}>0</Text> Playlists
+          </Text>
+          <Text style={style.statTxt}>
+            <Text style={style.statNum}>0</Text> Liked Songs
+          </Text>
+        </View>
+      </View>
+
+      <Text style={style.likedSongsTitle}>Your Liked Songs</Text>
+      <FlatList
+        data={likedSongs}
+        renderItem={({ item }) => (
+          <SongListCard
+            artwork={item.artwork}
+            title={item.title}
+            artist={item.artist}
+          />
+        )}
+        keyExtractor={(item) => item.id}
+        contentContainerStyle={style.songList}
+      />
+    </View>
+  );
 }
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingHorizontal: 20,
+    paddingTop: 20,
+  },
+
+  profileContainer: {
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+
+  profilePic: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    marginBottom: 10,
+  },
+
+  userName: {
+    fontSize: 20,
+    fontWeight: '600',
+    color: '#000',
+    marginBottom: 10,
+  },
+
+  stats: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '60%',
+  },
+
+  statTxt: {
+    fontSize: 14,
+    color: '#000',
+    textAlign: 'center',
+  },
+
+  statNum: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+
+  likedSongsTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginVertical: 10,
+  },
+
+  songList: {
+    paddingBottom: 20,
+  },
+
+});

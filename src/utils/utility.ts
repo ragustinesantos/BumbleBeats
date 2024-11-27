1847c6aa9f2ef9c22d75f1ecb572e05da87e016f
Raymond Santos <ragustinesantos@gmail.com>
2024-11-26 17:08:29 -0700

    11-26-2024 Update: - Added password hashing utility functions in utility.ts - Updated packages with bcrypt

--- src/utils/utility.ts
+++ src/utils/utility.ts
@@ -1,3 +1,5 @@
+import bcrypt from 'bcrypt';
+
 export interface TrackObject {
   id: number;
   title: string;
@@ -72,3 +74,28 @@ export const defaultUser: User = {
   playlists: [],
   recentlyPlayed: [],
 };
+
+// Encrypts inputted password and returns the hashed password
+export async function hashPassword(password: string) {
+  try {
+    const hashRounds = 10;
+
+    const hashedPassword = await bcrypt.hash(password, hashRounds);
+
+    return hashedPassword;
+  } catch (error) {
+    console.log(`Password Error ${error}`);
+  }
+}
+
+// Verifies if provided password matches with hashed password
+// returns true or false
+export async function verifyPassword(passwordInput: string, hashedPassword: string) {
+  try {
+    const isVerified = await bcrypt.compare(passwordInput, hashedPassword);
+
+    return isVerified;
+  } catch (error) {
+    console.log(`Password Error ${error}`);
+  }
+}
